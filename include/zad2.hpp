49f074e5c1d68e6cb58dea52fb28c151d8ac17f6
zakfilip <70323056+zakfilip@users.noreply.github.com>
2021-01-14 13:36:55 +0100

    Update zad2.hpp

--- include/zad2.hpp
+++ include/zad2.hpp
@@ -6,4 +6,27 @@
 
 // tutaj klasa Penne
 
+class Penne : public Makaron
+{
+public:
+	double ileMaki(unsigned P) const override
+	{
+		double wartosc = 5;
+		return wartosc;
+	}
+private:
+};
+
 // tutaj definicja metody gotujMakaron
+
+Makaron::gotujMakaron(const std::string& in)
+{
+	if (in.front() == in.back())
+	{
+		return new Tagliatelle(3.14, 0.42, 0.1);
+	}
+	else
+	{
+		return new Penne;
+	}
+}
